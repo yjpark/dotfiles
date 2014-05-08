b55028915423b015dcaa28ebe2c3d8ff1a81cc4b
YJ Park <yjpark@gmail.com>
2014-05-08 11:32:39 +0800

    adding oh-my-fish bootstrapper

--- bootstrap/fetch-all-bootsrap.sh
+++ bootstrap/fetch-all-bootsrap.sh
@@ -2,5 +2,6 @@
 
 curl -vLo- https://bit.ly/janus-bootstrap > ~/.dotfiles/bootstrap/bootstrap-janus.sh
 curl -vsL get.freshshell.com > ~/.dotfiles/bootstrap/bootstrap-fresh.sh
+curl -vL https://github.com/bpinto/oh-my-fish/raw/master/tools/install.fish > ~/.dotfiles/bootstrap/bootstrap-oh-my-fish.sh
 
 chmod 755 ~/.dotfiles/*.sh
