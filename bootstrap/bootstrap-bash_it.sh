ff8f3fb2f95561c673bb8af06267bf4c71ce8d7e
YJ Park <yjpark@gmail.com>
2014-05-08 13:28:18 +0800

    adjust path

--- bootstrap/bootstrap-bash_it.sh
+++ bootstrap/bootstrap-bash_it.sh
@@ -2,3 +2,7 @@
 
 git clone https://github.com/revans/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
+
+mkdir -p ~/.bash_it/aliases/enabled
+mkdir -p ~/.bash_it/plugins/enabled
+mkdir -p ~/.bash_it/completion/enabled
