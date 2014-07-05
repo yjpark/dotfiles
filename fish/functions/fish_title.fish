34526ca3e96327121167426767b381cc27532011
YJ Park <yjpark@gmail.com>
2014-07-05 22:33:31 +0800

    better title in tmux+fish

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,7 +1,8 @@
 function fish_title
   if [ "fish" != $_ ]
-    tmux rename-window $_
-    echo $_
+    set title (echo $_ | cut -b 1-24)
+    tmux rename-window $title
+    echo $title
   end
 end
 
