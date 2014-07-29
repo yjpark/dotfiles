403bfb276742821348e8dd45ea7b20c40da7b8bf
YJ Park <yjpark@gmail.com>
2014-07-29 14:07:05 +0800

    git bash's ps doesn't support -p

--- bash/custom.windows/ssh-agent.bash
+++ bash/custom.windows/ssh-agent.bash
@@ -4,9 +4,8 @@ if [[ -e $HOME/.sshagent.conf ]]; then
 . $HOME/.sshagent.conf
 fi
 
-if `ps -p ${SSH_AGENT_PID}>/dev/null`;then true;
-else
-ssh-agent >| $HOME/.sshagent.conf
-. $HOME/.sshagent.conf
-ssh-add ~/.ssh/id_dsa
-fi
+start_ssh_agent() {
+    ssh-agent >| $HOME/.sshagent.conf
+    . $HOME/.sshagent.conf
+    ssh-add ~/.ssh/id_rsa
+}
