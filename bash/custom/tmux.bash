f39a2886d843d6df7808c1fee6def6dc0cd74d55
YJ Park <yjpark@gmail.com>
2014-07-29 13:40:29 +0800

    tmux ssh fix

---
+++ bash/custom/tmux.bash
@@ -0,0 +1,10 @@
+
+tmux_fix_ssh_agent() {
+  for key in SSH_AUTH_SOCK SSH_CONNECTION SSH_CLIENT; do
+    if (tmux show-environment | grep "^${key}" > /dev/null); then
+      value=`tmux show-environment | grep "^${key}" | sed -e "s/^[A-Z_]*=//"`
+      export ${key}="${value}"
+    fi
+  done
+}
+
