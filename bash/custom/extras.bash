188bddb37be6a596cdac0a5a81bc26789feac91e
YJ Park <yjpark@gmail.com>
2014-07-30 14:18:26 +0800

    setup bash with extras

---
+++ bash/custom/extras.bash
@@ -0,0 +1,2 @@
+. ~/.bash_it/extras/bashmarks/bashmarks.sh
+. ~/.bash_it/extras/z/z.sh
--- fish/aliases/tmux.fish
+++ fish/aliases/tmux.fish
@@ -1,3 +1,4 @@
 alias tmux "tmux -u -2"
 alias tf tmux_fix_ssh_agent
 alias tc tmux-copy
+alias ta "tmux attach"
