49f3fae0721d998b1f057546767781ead59b3180
YJ Park <yjpark@gmail.com>
2014-08-02 23:03:13 +0800

    update bash fzf cmds

---
+++ bash/custom/fzf-cmd.bash
@@ -0,0 +1,79 @@
+# https://github.com/junegunn/fzf/wiki/examples
+#
+# Opening files
+#
+# fe [FUZZY PATTERN] - Open the selected file with the default editor
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fe() {
+      local file
+        file=$(fzf --query="$1" --select-1 --exit-0)
+          [ -n "$file"  ] && ${EDITOR:-vim} "$file"
+
+}
+
+# Equivalent to above, but opens it with `open` command
+fo() {
+      local file
+        file=$(fzf --query="$1" --select-1 --exit-0)
+          [ -n "$file"  ] && open "$file"
+}
+
+
+# Changing directory
+# fd - cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-*} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
+# fda - including hidden directories
+fda() {
+  local dir
+  dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
+}
+
+# cdf - cd into the directory of the selected file
+cdf() {
+   local file
+   local dir
+   file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
+}
+
+# fh - repeat history
+fh() {
+   eval $(([ -n "$ZSH_NAME"  ] && fc -l 1 || history) | fzf +s | sed 's/ *[0-9]* *//')
+}
+
+# fkill - kill process
+fkill() {
+  ps -ef | sed 1d | fzf -m | awk '{print $2}' | xargs kill -${1:-9}
+}
+
+# fbr - checkout git branch
+fbr() {
+  local branches branch
+  branches=$(git branch) &&
+  branch=$(echo "$branches" | fzf +s +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //")
+}
+
+# fco - checkout git commit
+fco() {
+  local commits commit
+  commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
+  commit=$(echo "$commits" | fzf +s +m -e) &&
+  git checkout $(echo "$commit" | sed "s/ .*//")
+}
+
+# Integration with z, like normal z when used with arguments but displays an fzf prompt when used without.
+unalias z
+z() {
+  if [[ -z "$*" ]]; then
+    cd "$(_z -l 2>&1 | sed -n 's/^[ 0-9.,]*//p' | fzf)"
+  else
+    _z "$@"
+  fi
+}
