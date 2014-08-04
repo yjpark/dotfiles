2b501361058d94ce8c5910407b2444f27a1da79e
YJ Park <yjpark@gmail.com>
2014-08-04 23:41:32 +0800

    remove param from the fzf commands

--- fish/custom/fzf_cmd.fish
+++ fish/custom/fzf_cmd.fish
@@ -1,5 +1,5 @@
 function ,o
-    fzf --query="$argv" --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
+    fzf --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
     set result (cat /tmp/fzf_fish.last)
     if test $result
         open (cat /tmp/fzf_fish.last)
@@ -7,7 +7,7 @@ function ,o
 end
 
 function ,e
-    fzf --query="$argv" --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
+    fzf --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
     set result (cat /tmp/fzf_fish.last)
     if test $result
         vim (cat /tmp/fzf_fish.last)
@@ -16,7 +16,7 @@ end
 
 function ,d
     find . -path '*/\.*' -prune -o -type d -print 2> /dev/null | \
-    fzf --query="$argv" --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
+    fzf --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
     set result (cat /tmp/fzf_fish.last)
     if test $result
         cd (cat /tmp/fzf_fish.last)
@@ -25,7 +25,7 @@ end
 
 function ,da
     find . -type d 2> /dev/null | \
-    fzf --query="$argv" --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
+    fzf --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
     set result (cat /tmp/fzf_fish.last)
     if test $result
         cd (cat /tmp/fzf_fish.last)
@@ -33,7 +33,7 @@ function ,da
 end
 
 function ,df
-    fzf --query="$argv" --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
+    fzf --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
     set result (cat /tmp/fzf_fish.last)
     if test $result
         cd (dirname (cat /tmp/fzf_fish.last))
@@ -41,11 +41,11 @@ function ,df
 end
 
 function ,kill
-    ps -ef | sed 1d | fzf --query="$argv" --select-1 --exit-0 +m -x | awk '{print $2}' | xargs kill
+    ps -ef | sed 1d | fzf --select-1 --exit-0 +m -x | awk '{print $2}' | xargs kill
 end
 
 function j
-    cat $HOME/.z | sed "s/|.*//" | fzf --query "$argv" --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
+    cat $HOME/.z | sed "s/|.*//" | fzf --select-1 --exit-0 +m -x > /tmp/fzf_fish.last
     set result (cat /tmp/fzf_fish.last)
     if test $result
         cd (cat /tmp/fzf_fish.last)
