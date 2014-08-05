cc77a45d63cae33b72928407922f6e0926eacb91
YJ Park <yjpark@gmail.com>
2014-08-05 12:32:24 +0800

    update windows settings

--- bash/custom.windows/windows.bash
+++ bash/custom.windows/windows.bash
@@ -3,6 +3,7 @@ export LANG="en_US.UTF-8"
 
 unalias grep
 
-//Powerline is not working properly with color and cursor pos
-//under conemu
-export PROMPT_COMMAND="prompt_command"
+#Powerline is not working properly with color and cursor pos
+#under conemu
+#Seems bash_it's prompt is really slow under windows, so stick to powerline.
+#export PROMPT_COMMAND="prompt_command"
