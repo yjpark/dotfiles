cac5510aa21a833b2fd439f8533cf37ee13ab085
YJ Park <yjpark@gmail.com>
2014-12-02 17:30:16 +0800

    add time to bash prompt

--- bash/custom/powershell.bash
+++ bash/custom/powershell.bash
@@ -1,6 +1,6 @@
 function _update_ps1()
 {
-    export PS1="$(~/bin/powerline-shell_bash.py $?)"
+    export PS1="$(date '+%T') $(~/bin/powerline-shell_bash.py $?)"
 }
 
 export PROMPT_COMMAND="_update_ps1"
