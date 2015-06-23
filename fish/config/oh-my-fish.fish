f3f021db257ff8f4c519de596706a479c3cc139a
YJ Park <yjpark@gmail.com>
2015-06-23 18:32:27 +0800

    update to latest oh-my-fish

--- fish/config/oh-my-fish.fish
+++ fish/config/oh-my-fish.fish
@@ -1,16 +1,20 @@
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
-# Theme
-set fish_theme agoster
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set fish_plugins git brew node python rvm
-
 # Path to your custom folder (default path is $FISH/custom)
 set fish_custom ~/.config/fish/custom
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+# Theme
+Theme "agnoster"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+Plugin "brew"
+Plugin "node"
+Plugin "python"
+Plugin "rvm"
+Plugin "theme"
