1c519d72fe7118ad814323503b4a22f2df7019a0
YJ Park <yjpark@gmail.com>
2015-06-28 11:58:52 +0800

    fix toggle color plugin with newer firefox

--- pentadactyl/plugins/toggle-color.js
+++ pentadactyl/plugins/toggle-color.js
@@ -2,12 +2,14 @@ group.commands.add(["toggleusedocumentcolor"],
     "Toggle the value of use_document_color",
     function(args) {
         var udc=prefs.get("browser.display.use_document_colors", true);
+        var dcu = prefs.get("browser.display.document_color_use", 1);
         var yjpark_color_index = prefs.get("browser.display.yjpark_color_index", 0);
         var log = "before: udc = " + udc + ", color_index = " + yjpark_color_index;
         var fg = "#000000";
         var bg = "#ffffff";
         if (udc) {
             udc = false;
+            dcu = 2;
             fg = "#839496";
             bg = "#002b36";
             yjpark_color_index = 0;
@@ -19,6 +21,7 @@ group.commands.add(["toggleusedocumentcolor"],
             } else if (yjpark_color_index >= 1) {
                 //dactyl.clipboardWrite("BBBB");
                 udc = true;
+                dcu = 1;
                 fg = "#000000";
                 bg = "#ffffff";
             }
@@ -26,6 +29,8 @@ group.commands.add(["toggleusedocumentcolor"],
         }
         //dactyl.clipboardWrite(log + ", after: udc = " + udc + ", color_index = " + yjpark_color_index, true);
         prefs.set("browser.display.use_document_colors", udc);
+        //Override the colors specified by the page with my selections above
+        prefs.set("browser.display.document_color_use", dcu);
         prefs.set("browser.display.background_color", bg);
         prefs.set("browser.display.foreground_color", fg);
         prefs.set("browser.display.yjpark_color_index", yjpark_color_index);
