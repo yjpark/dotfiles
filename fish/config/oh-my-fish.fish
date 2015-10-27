88c60f9f525c93d53fdc32e6fe0e60c1fb191fd1
YJ Park <yjpark@gmail.com>
2015-10-27 20:52:34 +0800

    follow oh my fish changes, add osx automators to export pdf to images

--- fish/config/oh-my-fish.fish
+++ fish/config/oh-my-fish.fish
@@ -1,5 +1,11 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+# Path to Oh My Fish install.
+set -gx OMF_PATH $HOME/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG /Users/yjpark/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
 
 # Path to your custom folder (default path is $FISH/custom)
 set fish_custom ~/.config/fish/custom
