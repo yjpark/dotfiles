5402580c52a4877b59a2306779eff5ee1d1caa9d
YJ Park <yjpark@gmail.com>
2016-03-16 17:29:00 +0800

    update color

--- fish/config/fisherman.fish
+++ fish/config/fisherman.fish
@@ -1,3 +1,8 @@
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
+
+set -U fish_color_normal    normal
+set -U fish_color_command   yellow
+set -U fish_color_param     cyan
+
 source $fisher_home/config.fish
