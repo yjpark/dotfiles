1e7c1dd1437b0219bd72487cff2764ac96e699da
YJ Park <yjpark@gmail.com>
2016-05-13 14:58:57 +0800

    use ndk r10e

--- fish/config.osx/android-ndk.fish
+++ fish/config.osx/android-ndk.fish
@@ -1,2 +1,2 @@
-set -x PATH $PATH ~/Applications/android-ndk-r10b/
-set -x PATH $PATH ~/Applications/android-ndk-r10b/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin/
+set -x PATH $PATH ~/Applications/android-ndk-r10e/
+set -x PATH $PATH ~/Applications/android-ndk-r10e/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin/
