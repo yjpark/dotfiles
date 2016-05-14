6ffb401f7dca11be9159364d81a175c6f79662c9
YJ Park <yjpark@gmail.com>
2016-05-14 13:42:10 +0800

    bugfix with msys2 fish

---
+++ fish/windows/fish.bashrc
@@ -0,0 +1,2 @@
+alias f="export HOME=/d/yjpark ; cd /d/yjpark ; mkdir -p /tmp/fish.yjpark ; XDG_RUNTIME_DIR=/tmp/fish.yjpark/ fish"
+alias b="export HOME=/d/yjpark ; cd /d/yjpark ; bash"
--- fish/windows/fix-key-bindings.fish
+++ fish/windows/fix-key-bindings.fish
@@ -28,3 +28,17 @@ set -x fish_color_error red --bold
 set -x fish_color_escape cyan
 set -x fish_color_history_current cyan
 set -x fish_color_host -o cyan
+
+# https://github.com/fish-shell/fish-shell/issues/1569
+# https://github.com/fish-shell/fish-shell/issues/952
+function x86
+    echo '(x86)'
+end
+function X86
+    echo '(X86)'
+end
+
+# https://github.com/fish-shell/fish-shell/issues/1887
+# Need to setup the mintty config:
+# - Terminal -> xterm-256color
+# - Keys -> Backarrow sends ^H
