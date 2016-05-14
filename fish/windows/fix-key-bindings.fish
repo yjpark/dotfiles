ffece5844c8c920d7fd6ebd38b8da7d8c83615f6
YJ Park <yjpark@gmail.com>
2016-05-14 13:05:39 +0800

    bugfix with msys2 fish

--- fish/windows/fix-key-bindings.fish
+++ fish/windows/fix-key-bindings.fish
@@ -23,3 +23,8 @@ bind \x5b\x44 backward-char
 
 alias a "ag --smart-case --search-files"
 
+# https://github.com/fish-shell/fish-shell/issues/1335
+set -x fish_color_error red --bold
+set -x fish_color_escape cyan
+set -x fish_color_history_current cyan
+set -x fish_color_host -o cyan
