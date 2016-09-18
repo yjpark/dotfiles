8258269211dfda054322d4a4261bec137b214370
YJ Park <yjpark@gmail.com>
2016-09-18 13:40:32 +0800

    update nix setting

--- fish/config.osx/nix.fish
+++ fish/config.osx/nix.fish
@@ -1,2 +1,6 @@
 set -x NIX_REMOTE ""
 set -x NIX_CURL_FLAGS "-x $http_proxy"
+
+alias ns "nix-shell --command fish"
+bass source ~/.nix-profile/etc/profile.d/nix.sh
+
--- fish/custom/bass_aliases.fish
+++ fish/custom/bass_aliases.fish
@@ -2,6 +2,6 @@ function nvm
     bass source ~/.external/tools/nvm/nvm.sh ';' nvm $argv
 end
 
-function nix
+function _nix
     bass source ~/.nix-profile/etc/profile.d/nix.sh
 end
