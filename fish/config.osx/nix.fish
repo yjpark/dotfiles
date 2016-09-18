b95a2fe8a0ed4f81d93a277fb049ce527d0e010f
YJ Park <yjpark@gmail.com>
2016-09-18 13:16:41 +0800

    setup nix to use proxy

---
+++ fish/config.osx/nix.fish
@@ -0,0 +1,2 @@
+set -x NIX_REMOTE ""
+set -x NIX_CURL_FLAGS "-x $http_proxy"
--- fish/custom/bass_aliases.fish
+++ fish/custom/bass_aliases.fish
@@ -2,3 +2,6 @@ function nvm
     bass source ~/.external/tools/nvm/nvm.sh ';' nvm $argv
 end
 
+function nix
+    bass source ~/.nix-profile/etc/profile.d/nix.sh
+end
