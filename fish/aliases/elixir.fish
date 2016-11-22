e296e70acec9baa7665ec6b189478f162b3d6b06
YJ Park <yjpark@gmail.com>
2016-11-22 11:58:36 +0800

    some set for elixir usage

---
+++ fish/aliases/elixir.fish
@@ -0,0 +1,3 @@
+alias set-elixir-cn-mirror-on "set -gx HEX_MIRROR https://hex.elixir-cn.org ; set -gx HEX_CDN https://hex.elixir-cn.org
+alias set-elixir-cn-mirror-off "set -ge HEX_MIRROR; set -ge HEX_CDN"
+
