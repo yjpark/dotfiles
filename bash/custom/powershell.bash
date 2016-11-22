4d6e34b5e54b58d64d654116b8c209e8dd012788
YJ Park <yjpark@gmail.com>
2016-11-22 19:50:32 +0800

    temp fix for powerline color for bash

--- bash/custom/powershell.bash
+++ bash/custom/powershell.bash
@@ -1,6 +1,6 @@
 function _update_ps1()
 {
-    export PS1="$(date '+%T') $(~/bin/powerline-shell_bash.py $?)"
+    export PS1="$(date '+%T') [bash] $(~/bin/powerline-shell_fish.py $?)"
 }
 
 export PROMPT_COMMAND="_update_ps1"
