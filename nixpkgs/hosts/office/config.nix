26c6440fc4ae6b70346239b7cce77f21e34261f2
YJ Park <yjpark@gmail.com>
2016-11-24 15:39:48 +0000

    add office nixpkgs setting

---
+++ nixpkgs/hosts/office/config.nix
@@ -0,0 +1,22 @@
+{
+  allowUnfree = true;
+
+  firefox = {
+    enableGoogleTalkPlugin = true;
+    enableAdobeFlash = true;
+  };
+
+  chromium = {
+    enablePepperFlash = true; # Chromium removed support for Mozilla (NPAPI) plugins so Adobe Flash no longer works
+    enablePepperPDF = true;
+  };
+
+  packageOverrides = pkgs_: with pkgs_; {
+    all = with pkgs; buildEnv {
+      name = "all";
+      paths = [
+        firefox
+        chromium
+      ];
+    };
+  };}
