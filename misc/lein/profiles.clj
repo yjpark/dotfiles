782003dc4fa1f91366e1ced08b0f674017be50ed
YJ Park <yjpark@gmail.com>
2017-01-02 22:19:51 +0800

    update lein config

--- misc/lein/profiles.clj
+++ misc/lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+{:user {:plugins [[cider/cider-nrepl "0.14.0"]
                   [refactor-nrepl "1.1.0"]
                   [jonase/eastwood "0.2.3"]]}}
 
