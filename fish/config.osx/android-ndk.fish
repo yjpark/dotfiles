0a41d4a9a0614d312417932ccb2fc88bc9f5b4fc
YJ Park <yjpark@gmail.com>
2017-04-30 12:56:11 +0800

    add path check for android stuffs, remove karabiner setting

--- fish/config.osx/android-ndk.fish
+++ fish/config.osx/android-ndk.fish
@@ -1,2 +1,4 @@
-set -x PATH $PATH ~/Applications/android-ndk/
-set -x PATH $PATH ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin/
+if test -d ~/Applications/android-ndk
+    set -x PATH $PATH ~/Applications/android-ndk/
+    set -x PATH $PATH ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin/
+end
--- fish/config.osx/android-sdk.fish
+++ fish/config.osx/android-sdk.fish
@@ -1,2 +1,4 @@
-set -x ANDROID_SDK_HOME "$HOME/Applications/android-sdk/platform-tools/"
-set -x PATH $PATH ~/Applications/android-sdk/platform-tools
+if test -d ~/Applications/android-sdk
+    set -x ANDROID_SDK_HOME "$HOME/Applications/android-sdk/platform-tools/"
+    set -x PATH $PATH ~/Applications/android-sdk/platform-tools
+end
