a7e89b5c01db792b14d14b4f817a6c0b1e2ff537
YJ Park <yjpark@gmail.com>
2017-07-08 18:05:55 +0800

    update fundle setting

--- fish/config.linux.ubuntu/windows-wsl.fish
+++ fish/config.linux.ubuntu/windows-wsl.fish
@@ -1,5 +1,4 @@
 if grep -q Microsoft /proc/version
     functions --erase fish_prompt
-    fundle plugin "oh-my-fish/theme-gitstatus"
     start-ssh-agent
 end
--- fish/config/fundle-pkgs.fish
+++ fish/config/fundle-pkgs.fish
@@ -1,3 +1,4 @@
 fundle plugin 'tuvistavie/fish-ssh-agent'
+fundle plugin "oh-my-fish/theme-gitstatus"
 
 # Need to run `fundle install` to actually install them
