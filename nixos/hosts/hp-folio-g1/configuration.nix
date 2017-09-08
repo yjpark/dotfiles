5c71775cbd7dee014ec69ab797791214cd02c0f7
YJ Park <yjpark@gmail.com>
2017-09-08 17:05:32 +0000

    update nixos setting

--- nixos/hosts/hp-folio-g1/configuration.nix
+++ nixos/hosts/hp-folio-g1/configuration.nix
@@ -24,6 +24,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  services.xserver.synaptics.enable = true;
+
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
