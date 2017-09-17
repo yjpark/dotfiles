5f910a79dcb51ba4137b954ad77c620fffda158e
YJ Park <yjpark@gmail.com>
2017-09-18 00:17:05 +0800

    clear_buffer properly

--- fish/functions/clear_buffer.fish
+++ fish/functions/clear_buffer.fish
@@ -1,5 +1,7 @@
 function clear_buffer
     echo -e -n "\\0033c"
+    # https://superuser.com/questions/59682/how-can-i-clear-the-scrollback-buffer-in-konsole-from-bash
+    echo -e -n "\e[3J"
     if set -q TMUX
         tmux clear-history
     end
