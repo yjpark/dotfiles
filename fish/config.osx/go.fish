8d494c81516e67bbc4116f0e5242c998d4405ae7
YJ Park <yjpark@gmail.com>
2017-10-05 21:41:21 +0800

    update go related setting

---
+++ fish/config.osx/go.fish
@@ -0,0 +1,4 @@
+if test -d /usr/local/opt/go/libexec/bin
+    #set -x PATH $PATH /usr/local/opt/go/libexec/bin
+    set -x GOROOT /usr/local/opt/go/libexec
+end
--- fish/config/go.fish
+++ fish/config/go.fish
@@ -1,4 +1,7 @@
 if test -d ~/.go
     set -x GOPATH ~/.go
+end
+
+if test -d ~/.go/bin
     set -x PATH $PATH ~/.go/bin
 end
