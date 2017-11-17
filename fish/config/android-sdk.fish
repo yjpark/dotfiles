3fe09f55d628457c89083bbbe966c6104527450c
YJ Park <yjpark@gmail.com>
2017-11-17 14:45:20 +0800

    fix fish setting

--- fish/config/android-sdk.fish
+++ fish/config/android-sdk.fish
@@ -1,4 +1,7 @@
 if test -d ~/Applications/android-sdk
-    set -x ANDROID_SDK_HOME "$HOME/Applications/android-sdk/platform-tools/"
+    set -x ANDROID_SDK_HOME "$HOME/Applications/android-sdk/"
+end
+
+if test -d ~/Applications/android-sdk/platform-tools
     set -x PATH $PATH ~/Applications/android-sdk/platform-tools
 end
