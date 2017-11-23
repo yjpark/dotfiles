33d18f472d7a2511db86c54dfee7ec541fe227aa
YJ Park <yjpark@gmail.com>
2017-11-23 20:59:45 +0800

    cleanup nixos setting

--- nixos/settings/nixpkgs_fork.nix
+++ nixos/settings/nixpkgs_fork.nix
@@ -1,9 +1,12 @@
 { config, pkgs, ... }:
 {
+  # https://nixos.org/nixos/manual/options.html#opt-nix.nixPath
+  # https://github.com/NixOS/nixpkgs/issues/25264
   # https://github.com/Mic92/dotfiles/blob/master/nixos/configuration.nix
   nix.nixPath = [
     "nixpkgs=/home/yjpark/projects/forks/nixpkgs"
     "nixos-config=/etc/nixos/configuration.nix"
+    "/nix/var/nix/profiles/per-user/root/channels"
     # "nixpkgs-overlays=/home/joerg/git/nixos-configuration/overlays"
   ];
 }
