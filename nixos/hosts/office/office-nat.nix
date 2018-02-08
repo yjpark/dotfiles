e9b1d48b84d7a001c3fdb1563ea4e11ae7fdc434
YJ Park <yjpark@gmail.com>
2018-02-08 12:35:17 +0800

    more net interface change

--- nixos/hosts/office/office-nat.nix
+++ nixos/hosts/office/office-nat.nix
@@ -5,7 +5,7 @@
     enable = true;
     internalIPs = [ "10.0.2.0/24" ];
     internalInterfaces = [ "eth0" ];
-    externalInterface = "wlp8s0";
+    externalInterface = "wlan0";
     forwardPorts = [
       # { sourcePort = 53; destination = "127.0.0.1:53"; proto = "tcp"; }
       # { sourcePort = 53; destination = "127.0.0.1:53"; proto = "udp"; }
@@ -25,10 +25,10 @@
     allowedTCPPorts = [ 22 53 1100 1101 1102 1103 1104 8126 ];
     allowedUDPPorts = [ 53 ];
     extraCommands = ''
-        iptables -t nat -A POSTROUTING -s 10.0.2.0/24 -o wlp8s0 -j MASQUERADE
+        iptables -t nat -A POSTROUTING -s 10.0.2.0/24 -o wlan0 -j MASQUERADE
     '';
     extraStopCommands = ''
-      iptables -t nat -D POSTROUTING -s 10.0.2.0/24 -o wlp8s0 -j MASQUERADE
+      iptables -t nat -D POSTROUTING -s 10.0.2.0/24 -o wlan0 -j MASQUERADE
     '';
   };
 
