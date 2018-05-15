6f9fff854ba9c982f7263497776a8f8c7c876ded
YJ Park <yjpark@gmail.com>
2018-05-15 11:30:53 +0800

    alias for dotnet

---
+++ fish/aliases/dotnet.fish
@@ -0,0 +1,4 @@
+alias paket-5.156.1='mono ~/.local/share/NuGet/Cache/Paket/5.156.1/paket.exe'
+alias dr='cb ; dotnet restore'
+alias db='cb ; dotnet build --no-restore'
+alias dr='cb ; dotnet run --no-build --no-restore'
--- fish/aliases/fsharp.fish
+++
@@ -1,1 +0,0 @@
-alias paket-5.156.1='mono ~/.local/share/NuGet/Cache/Paket/5.156.1/paket.exe'
