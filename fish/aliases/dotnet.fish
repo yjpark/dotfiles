7061420060ad780e3243f0bf298ab37975a9fd2c
YJ Park <yjpark@gmail.com>
2018-05-15 15:50:00 +0800

    tweak alias name

--- fish/aliases/dotnet.fish
+++ fish/aliases/dotnet.fish
@@ -1,4 +1,4 @@
 alias paket-5.156.1='mono ~/.local/share/NuGet/Cache/Paket/5.156.1/paket.exe'
-alias dotnet-restore='cb ; dotnet restore'
-alias dotnet-build='cb ; dotnet build --no-restore'
-alias dotnet-run='cb ; dotnet run --no-build --no-restore'
+alias d.restore='cb ; dotnet restore'
+alias d.build='cb ; dotnet build --no-restore'
+alias d.run='cb ; dotnet run --no-build --no-restore'
