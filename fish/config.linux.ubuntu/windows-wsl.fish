d8c75a904814f4f0f84509fca9b77c43e972e8c8
YJ Park <yjpark@gmail.com>
2018-06-15 19:02:54 +0800

    window wsl setting

--- fish/config.linux.ubuntu/windows-wsl.fish
+++ fish/config.linux.ubuntu/windows-wsl.fish
@@ -1,6 +1,8 @@
 if grep -q Microsoft /proc/version
     # functions --erase fish_prompt
     set -x DOCKER_HOST tcp://127.0.0.1:2375
+    set -x PATH ~/.dotfiles/bin/linux.ubuntu.wsl $PATH
+
     setup-ssh-agent
 end
 
