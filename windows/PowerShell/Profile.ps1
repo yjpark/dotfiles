ea50d399dacebec687916a971cdda0f901098d05
YJ Park <yjpark@gmail.com>
2018-06-15 19:50:22 +0800

    more powershell setting

--- windows/PowerShell/Profile.ps1
+++ windows/PowerShell/Profile.ps1
@@ -8,6 +8,9 @@ Set-Alias -Name ... -Value ~\Documents\WindowsPowerShell\aliases\cd.uuu.ps1
 Set-Alias -Name .... -Value ~\Documents\WindowsPowerShell\aliases\cd.uuuu.ps1
 Set-Alias -Name ..... -Value ~\Documents\WindowsPowerShell\aliases\cd.uuuuu.ps1
 
+Set-Alias -Name g -Value git
+Set-Alias -Name d -Value docker
+
 Set-Alias -Name gg.bit -Value ~\Documents\WindowsPowerShell\aliases\gg.bit.ps1
 
 Set-Alias -Name d.restore -Value ~\Documents\WindowsPowerShell\aliases\d.restore.ps1
