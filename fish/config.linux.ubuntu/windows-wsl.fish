b7ec3f5fa3aa8f45c28cbac625de9d2301fd0d20
YJ Park <yjpark@gmail.com>
2018-06-15 19:01:03 +0800

    more powershell setting

--- fish/config.linux.ubuntu/windows-wsl.fish
+++ fish/config.linux.ubuntu/windows-wsl.fish
@@ -1,4 +1,7 @@
 if grep -q Microsoft /proc/version
     # functions --erase fish_prompt
+    set -x DOCKER_HOST tcp://127.0.0.1:2375
     setup-ssh-agent
 end
+
+
--- windows/PowerShell/Profile.ps1
+++ windows/PowerShell/Profile.ps1
@@ -1,9 +1,18 @@
 $env:Path += "$env:Path;C:\Users\yjpark\Documents\WindowsPowerShell\bin"
 
+Set-Alias -Name l -Value ls
 Set-Alias -Name ll -Value ls
+
+Set-Alias -Name .. -Value ~\Documents\WindowsPowerShell\aliases\cd.uu.ps1
+Set-Alias -Name ... -Value ~\Documents\WindowsPowerShell\aliases\cd.uuu.ps1
+Set-Alias -Name .... -Value ~\Documents\WindowsPowerShell\aliases\cd.uuuu.ps1
+Set-Alias -Name ..... -Value ~\Documents\WindowsPowerShell\aliases\cd.uuuuu.ps1
 
 Set-Alias -Name gg.bit -Value ~\Documents\WindowsPowerShell\aliases\gg.bit.ps1
 
 Set-Alias -Name d.restore -Value ~\Documents\WindowsPowerShell\aliases\d.restore.ps1
 Set-Alias -Name d.build -Value ~\Documents\WindowsPowerShell\aliases\d.build.ps1
 Set-Alias -Name d.run -Value ~\Documents\WindowsPowerShell\aliases\d.run.ps1
+
+# Import-Module "Oh-My-Powershell" -DisableNameChecking -NoClobber
+
