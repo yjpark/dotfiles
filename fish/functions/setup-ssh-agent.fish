fb45b0e2713a28002c48ee91c33a3e28c306904b
YJ Park <yjpark@gmail.com>
2018-06-16 15:35:13 +0800

    bugfix with setup-ssh-agent

--- fish/functions/setup-ssh-agent.fish
+++ fish/functions/setup-ssh-agent.fish
@@ -1,6 +1,5 @@
 setenv SSH_ENV $HOME/.ssh/environment
 
-
 function setup-ssh-agent
     if [ -f $SSH_ENV ]
         source $SSH_ENV > /dev/null
@@ -9,13 +8,15 @@ function setup-ssh-agent
         ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
         if [ $status -eq 0 ]
             check_ssh_identity
+        else
+            reset_ssh_agent
         end
     else
-        ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+        ps -ef | grep "ssh-agent -c" | grep -v grep | grep "ssh-agent -c" > /dev/null
         if [ $status -eq 0 ]
             check_ssh_identity
         else
-            echo "Initializing new SSH agent ..."
+            echo "Initializing new ssh-agent ..."
             ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
             echo "succeeded"
             chmod 600 $SSH_ENV 
@@ -25,17 +26,24 @@ function setup-ssh-agent
     end
 end
 
+function reset_ssh_agent
+    echo "Reset ssh-agent"
+    pkill -9 ssh-agent
+    rm $SSH_ENV
+    set -ex SSH_AGENT_PID
+    setup-ssh-agent
+end
+
 
 function check_ssh_identity
+    echo "Check ssh identity: $SSH_AGENT_PID $SSH_ENV"
     ssh-add -l | grep "The agent has no identities" > /dev/null
     if [ $status -eq 0 ]
         ssh-add
     else
         ssh-add -l | grep "Connection refused" > /dev/null
         if [ $status -eq 0 ]
-            set -ex SSH_ENV
-            set -ex SSH_AGENT_PID
-            setup-ssh-agent
+            reset_ssh_agent
         end
     end
 end
