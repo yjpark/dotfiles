e6b8bf71c5e975571debf1ae97e83fd34b060313
YJ Park <yjpark@gmail.com>
2018-08-19 10:20:43 +0800

    use windows' dotnet in wsl

--- fish/aliases/wsl.fish
+++
@@ -1,1 +0,0 @@
-alias wsl 'set -x LS_COLORS "" ; set -x DISPLAY "127.0.0.1:0.0"'
--- fish/config.linux.ubuntu/windows-wsl.fish
+++ fish/config.linux.ubuntu/windows-wsl.fish
@@ -3,7 +3,11 @@ if grep -q Microsoft /proc/version
     set -x DOCKER_HOST tcp://127.0.0.1:2375
     set -x PATH ~/.dotfiles/bin/linux.ubuntu.wsl $PATH
 
+    alias wsl 'set -x LS_COLORS "" ; set -x DISPLAY "127.0.0.1:0.0"'
+
     alias yarn 'yarn --cache-folder ~/.cache/yarn/v1'
+    alias dotnet '/mnt/c/Program\ Files/dotnet/dotnet.exe'
+    alias fake '~/win/.dotnet/tools/fake'
 
     setup-ssh-agent
 end
