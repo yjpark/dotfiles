2fa822cd7a8fedd8722f8edb2c26daaf3a7d2b4f
YJ Park <yjpark@gmail.com>
2018-10-22 18:40:35 +0800

    msbuild for linux and osx

---
+++ fish/aliases.linux/dotnet.fish
@@ -0,0 +1,2 @@
+alias msbuild='cb ; /usr/bin/msbuild'
+alias msbuild.restore='msbuild /t:restore'
---
+++ fish/aliases.osx/dotnet.fish
@@ -0,0 +1,2 @@
+alias msbuild='cb ; /Library/Frameworks/Mono.framework/Versions/Current/Commands/msbuild'
+alias msbuild.restore='msbuild /t:restore'
