f2e3c9bf5cb185cbb5ff4b3663d72514f8a45f30
YJ Park <yjpark@gmail.com>
2018-11-02 15:46:26 +0800

    update ndk setting

--- fish/config/android-ndk.fish
+++ fish/config/android-ndk.fish
@@ -2,6 +2,10 @@ if test -d ~/Applications/android-ndk
     set -x ANDROID_NDK_HOME ~/Applications/android-ndk/
 end
 
-if test -d ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin/
-    set -x PATH $PATH ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin/
+if test -d ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/
+    set -x PATH $PATH ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/
+end
+
+if test -d ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/
+    set -x PATH $PATH ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/
 end
