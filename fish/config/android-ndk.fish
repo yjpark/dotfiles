3c86be8d1ffbd9d2b9687070504a89f599d3f5cb
YJ Park <yjpark@gmail.com>
2018-11-02 15:49:49 +0800

    update ndk setting

--- fish/config/android-ndk.fish
+++ fish/config/android-ndk.fish
@@ -1,11 +1,12 @@
-if test -d ~/Applications/android-ndk
-    set -x ANDROID_NDK_HOME ~/Applications/android-ndk/
+if test -d ~/Applications/android-ndk-r18b
+    set -x ANDROID_NDK_HOME ~/Applications/android-ndk-r18b/
+    set -x PATH $PATH ~/Applications/android-ndk-r18b
 end
 
-if test -d ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/
-    set -x PATH $PATH ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/
+if test -d ~/Applications/android-ndk-r18b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/
+    set -x PATH $PATH ~/Applications/android-ndk-r18b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/
 end
 
-if test -d ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/
-    set -x PATH $PATH ~/Applications/android-ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/
+if test -d ~/Applications/android-ndk-r18b/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/
+    set -x PATH $PATH ~/Applications/android-ndk-r18b/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/
 end
