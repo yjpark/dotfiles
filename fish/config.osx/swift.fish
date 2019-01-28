865f2ddd2125461ba2f2c32b8cf13bd1cf827731
YJ Park <yjpark@gmail.com>
2019-01-28 20:26:38 +0800

    use latest swift in toolchain

--- fish/config.osx/swift.fish
+++ fish/config.osx/swift.fish
@@ -2,4 +2,5 @@
 
 if test -e /Library/Developer/Toolchains/swift-latest.xctoolchain
     set -x SOURCEKIT_TOOLCHAIN_PATH /Library/Developer/Toolchains/swift-latest.xctoolchain
+    set -x PATH /Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin $PATH
 end
