1d54f52977556406592d0d20979c0122cf358ff3
YJ Park <yjpark@gmail.com>
2019-01-28 20:19:55 +0800

    add swift toolchain setting for osx

---
+++ fish/config.osx/swift.fish
@@ -0,0 +1,5 @@
+# https://github.com/apple/sourcekit-lsp
+
+if test -e /Library/Developer/Toolchains/swift-latest.xctoolchain
+    set -x SOURCEKIT_TOOLCHAIN_PATH /Library/Developer/Toolchains/swift-latest.xctoolchain
+end
