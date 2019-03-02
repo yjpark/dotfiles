95f8b6e41e25fed23d25313b8f74672516123318
YJ Park <yjpark@gmail.com>
2019-03-02 13:05:54 +0800

    update env

--- fish/config.osx/macvim.fish
+++
@@ -1,6 +0,0 @@
-# set -x DYLD_FORCE_FLAT_NAMESPACE 1
-# set -x DYLD_INSERT_LIBRARIES /usr/local/Cellar/python/2.7.10_2/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib
-
-set -x EDITOR "/Applications/MacVim.app/Contents/MacOS/Vim -n"
-alias vim "/Applications/MacVim.app/Contents/MacOS/Vim -n"
-
