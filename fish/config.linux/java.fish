faf4cc09209d8c67d156f0bd3ba611f3814e9c9c
YJ Park <yjpark@gmail.com>
2019-06-14 13:45:56 +0800

    add oracle jdk alias

--- fish/config.linux/java.fish
+++ fish/config.linux/java.fish
@@ -5,6 +5,13 @@ function use-oracle-jdk-1.11
     end
 end
 
+function use-oracle-jdk-1.8
+    if test -d /opt/jdk1.8.0_211
+        set -gx JAVA_HOME "/opt/jdk1.8.0_211"
+        set -x PATH "/opt/jdk1.8.0_211/bin" $PATH
+    end
+end
+
 function use-openjdk-1.8
     if test -d /usr/lib/jvm/java-1.8.0-openjdk-amd64/bin
         set -gx JAVA_HOME "/usr/lib/jvm/java-1.8.0-openjdk-amd64"
