bb08e54e0aecffa2e79a8b8bd9bf7d47baee1f2c
YJ Park <yjpark@gmail.com>
2019-06-15 18:52:27 +0800

    termux fix

--- fish/config/ssh-agent.fish
+++ fish/config/ssh-agent.fish
@@ -1,4 +1,11 @@
-if test -e ~/.ssh/id_dsa
+if [ -f ~/.termux ]
+    setenv SSH_ENV $HOME/.ssh/environment
+    if [ -f $SSH_ENV ]
+        source $SSH_ENV > /dev/null
+    else
+        ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    end
+elif test -e ~/.ssh/id_dsa
     setup-ssh-agent
 else if test -e ~/.ssh/id_rsa
     setup-ssh-agent
