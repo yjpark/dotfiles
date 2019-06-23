5d4b63c7b3d8a979cec54350eea69f2c4397bc35
YJ Park <yjpark@gmail.com>
2019-06-23 17:00:08 +0800

    osx java config

---
+++ fish/config.osx/java.fish
@@ -0,0 +1,13 @@
+function use-oracle-jdk-8
+    if test -d /Library/Java/JavaVirtualMachines/jdk1.8.0_152.jdk/Contents/Home/bin
+        set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_152.jdk/Contents/Home"
+        set -x PATH "/Library/Java/JavaVirtualMachines/jdk1.8.0_152.jdk/Contents/Home/bin" $PATH
+    end
+end
+
+function use-oracle-jdk-11
+    if test -d /Library/Java/JavaVirtualMachines/jdk11.0.3.jdk/Contents/Home/bin
+        set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk11.0.3.jdk/Contents/Home"
+        set -x PATH "/Library/Java/JavaVirtualMachines/jdk11.0.3.jdk/Contents/Home/bin" $PATH
+    end
+end
