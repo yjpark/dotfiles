26b51e8559b040af3f851e3e65052e3850e647ff
YJ Park <yjpark@gmail.com>
2019-06-29 16:02:03 +0800

    add .local/bin and /snap/bin to path for bash

--- bash/custom/path.bash
+++ bash/custom/path.bash
@@ -1,1 +1,10 @@
 export PATH=/usr/bin:/usr/local/bin:/opt/bin:$PATH
+
+if [ -d "$HOME/.local/bin" ]; then
+    export PATH="$PATH:$HOME/.local/bin"
+fi
+
+if [ -d "$HOME/snap/bin" ]; then
+    export PATH="$PATH:$HOME/snap/bin"
+fi
+
