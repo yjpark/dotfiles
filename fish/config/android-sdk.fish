427ea2420a687ed861a92cbd762b070382b93605
YJ Park <yjpark@gmail.com>
2019-09-30 23:46:21 +0800

    small fix

--- fish/config/android-sdk.fish
+++ fish/config/android-sdk.fish
@@ -1,6 +1,7 @@
 if test -d ~/Applications/android-sdk
     set -x ANDROID_HOME "$HOME/Applications/android-sdk/"
     set -x ANDROID_SDK "$HOME/Applications/android-sdk/"
+    set -x ANDROID_SDK_ROOT "$HOME/Applications/android-sdk/"
 end
 
 if test -d ~/Applications/android-sdk/platform-tools
