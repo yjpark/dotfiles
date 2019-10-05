f756db40a729cd57a9e1f00fc14d3dd1c3d368ad
YJ Park <yjpark@gmail.com>
2019-10-05 21:17:43 +0800

    update nix fish setting

--- fish/config.linux.ubuntu/nix.fish
+++ fish/config.linux.ubuntu/nix.fish
@@ -1,4 +1,7 @@
+set -x NIX_REMOTE ""
+set -x NIX_CURL_FLAGS "-x $http_proxy"
+
+alias ns "nix-shell --command fish"
 if test -d ~/.nix-profile
     bass source ~/.nix-profile/etc/profile.d/nix.sh
-    set -x PATH $PATH ~/.nix-profile/bin/
 end
