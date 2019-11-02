87abfb5137e8e4f0fa9ef9d6778b86605a2be22d
YJ Park <yjpark@gmail.com>
2019-11-02 18:14:51 +0800

    add alias to use zerossl tool

---
+++ fish/aliases/zerossl.fish
@@ -0,0 +1,1 @@
+alias le.pl='docker run -it -v /home/yjpark/.dotfiles/private/secret/zerossl/data:/data -v /home/yjpark/.dotfiles/private/secret/zerossl/acme-challenge:/webroot -u (id -u) --rm zerossl/client'
