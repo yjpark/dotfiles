24d6e280272c1962b379f104d9af60dc9eab6683
YJ Park <yjpark@gmail.com>
2019-11-25 23:02:19 +0800

    fix protoc.dart

--- fish/aliases/protoc.fish
+++ fish/aliases/protoc.fish
@@ -1,1 +1,1 @@
-alias protoc.dart 'protoc --plugin=~/.dotfiles/external/protobuf/protobuf-dart_19.0.0-1/protoc_plugin/bin/protoc-gen-dart'
+alias protoc.dart 'protoc --plugin=$HOME/.dotfiles/external/protobuf/protobuf-dart_19.0.0-1/protoc_plugin/bin/protoc-gen-dart'
