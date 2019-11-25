08c7abbc40912c875a239dd735a2d8c68de1901e
YJ Park <yjpark@gmail.com>
2019-11-25 23:31:23 +0800

    add protoc.dart.init

--- fish/aliases/protoc.fish
+++ fish/aliases/protoc.fish
@@ -1,1 +1,2 @@
+alias protoc.dart.init 'cd $HOME/.dotfiles/external/protobuf/protobuf-dart_19.0.0-1/protoc_plugin/ ; pub get'
 alias protoc.dart 'protoc --plugin=$HOME/.dotfiles/external/protobuf/protobuf-dart_19.0.0-1/protoc_plugin/bin/protoc-gen-dart'
