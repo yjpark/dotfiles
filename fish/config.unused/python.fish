c87507cba584a90833bf7602db316369dddb5d2e
YJ Park <yjpark@gmail.com>
2020-01-13 00:38:47 +0800

    add anaconda.fish

--- fish/config.unused/python.fish
+++
@@ -1,6 +0,0 @@
-if test -d ~/Applications/anaconda3
-    . ~/Applications/anaconda3/etc/fish/conf.d/conda.fish
-else if test -d /anaconda3
-    . /anaconda3/etc/fish/conf.d/conda.fish
-end
-
---
+++ fish/config/anaconda.fish
@@ -0,0 +1,6 @@
+if test -d ~/Applications/anaconda3
+    source ~/Applications/anaconda3/etc/fish/conf.d/conda.fish
+else if test -d /anaconda3
+    source /anaconda3/etc/fish/conf.d/conda.fish
+end
+
