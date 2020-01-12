6c7667a1712157e6a750d238ee693ef25be51eaa
YJ Park <yjpark@gmail.com>
2020-01-13 00:22:29 +0800

    adjust path

--- fish/config/python.fish
+++ fish/config/python.fish
@@ -1,5 +1,5 @@
-if test -d ~/anaconda3
-    . ~/anaconda3/etc/fish/conf.d/conda.fish
+if test -d ~/Applications/anaconda3
+    . ~/Applications/anaconda3/etc/fish/conf.d/conda.fish
 else if test -d /anaconda3
     . /anaconda3/etc/fish/conf.d/conda.fish
 end
