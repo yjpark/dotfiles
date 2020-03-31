baebebe001f92b351529da725a55e7a661bc5470
YJ Park <yjpark@gmail.com>
2020-03-31 18:00:57 +0800

    disable conda.fish, some alias for rsync

--- fish/aliases/rsync.fish
+++ fish/aliases/rsync.fish
@@ -1,2 +1,3 @@
-alias rsync 'rsync -av --stats --progress'
-alias rsync.compress 'rsync -avz --stats --progress'
+alias rsync 'rsync -aHAXxv --numeric-ids --stats --progress -e "ssh -T -c arcfour -o Compression=no -x"'
+alias rsync.compress 'rsync -aHAXxv --numeric-ids --stats --progress  -e "ssh -T -c arcfour -o Compression=no -x" --compress'
+alias rsync.delete 'rsync -aHAXxv --numeric-ids --stats --progress  -e "ssh -T -c arcfour -o Compression=no -x" --delete'
