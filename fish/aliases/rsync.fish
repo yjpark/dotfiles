78dfd5e6dffeb00d5f4b032a0675f696f4e7c87a
YJ Park <yjpark@gmail.com>
2020-03-31 18:41:01 +0800

    cleanup aliases

--- fish/aliases/rsync.fish
+++ fish/aliases/rsync.fish
@@ -1,3 +1,3 @@
-alias rsync 'rsync -aHAXxv --numeric-ids --stats --progress -e "ssh -T -c arcfour -o Compression=no -x"'
-alias rsync.compress 'rsync -aHAXxv --numeric-ids --stats --progress  -e "ssh -T -c arcfour -o Compression=no -x" --compress'
-alias rsync.delete 'rsync -aHAXxv --numeric-ids --stats --progress  -e "ssh -T -c arcfour -o Compression=no -x" --delete'
+alias rsync.progress 'rsync -aHAXxv --numeric-ids --stats --progress -e "ssh -T -c arcfour -o Compression=no -x"'
+alias rsync.progress.compress 'rsync -aHAXxv --numeric-ids --stats --progress  -e "ssh -T -c arcfour -o Compression=no -x" --compress'
+alias rsync.progress.delete 'rsync -aHAXxv --numeric-ids --stats --progress  -e "ssh -T -c arcfour -o Compression=no -x" --delete'
