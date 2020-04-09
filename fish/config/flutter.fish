1f896967c14101800bd1557cee2f8d48aaeb6bc6
YJ Park <yjpark@gmail.com>
2020-04-10 00:38:06 +0800

    setup flutter cn mirror, install greenplum

--- fish/config/flutter.fish
+++ fish/config/flutter.fish
@@ -1,3 +1,6 @@
 if test -d ~/Applications/flutter/bin
     set -x PATH $PATH ~/Applications/flutter/bin/
+    # https://flutter.dev/community/china
+    set -x PUB_HOSTED_URL https://pub.flutter-io.cn
+    set -x FLUTTER_STORAGE_BASE_URL https://storage.flutter-io.cn
 end
