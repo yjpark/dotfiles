0981af600cf8bb2474f94545835b261ae208c9e7
YJ Park <yjpark@gmail.com>
2020-05-13 23:34:02 +0800

    update

--- fish/custom/bass_aliases.fish
+++ fish/custom/bass_aliases.fish
@@ -1,6 +1,6 @@
-# function nvm
-#     bass source ~/.external/tools/nvm/nvm.sh ';' nvm $argv
-# end
+function nvm
+    bass source ~/.nvm/nvm.sh ';' nvm $argv
+end
 
 function _nix
     bass source ~/.nix-profile/etc/profile.d/nix.sh
