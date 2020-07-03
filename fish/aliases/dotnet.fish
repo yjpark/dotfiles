3bd16eac641c5da8de4506a57f0fa76a5c14e72a
YJ Park <yjpark@gmail.com>
2020-07-03 13:00:15 +0800

    support polling mode for d.watch, needed for linked files

--- fish/aliases/dotnet.fish
+++ fish/aliases/dotnet.fish
@@ -3,6 +3,7 @@ alias d.build='cb ; dotnet build --no-restore'
 alias d.build.self='cb ; dotnet build --no-restore --no-dependencies'
 alias d.run='cb ; dotnet run --no-build --no-restore'
 alias d.watch='cb ; dotnet watch run --no-restore'
+alias d.watch.poll='set DOTNET_USE_POLLING_FILE_WATCHER = true ; cb ; dotnet watch run --no-restore'
 alias d.fable='cb ; dotnet fable yarn-start'
 alias d.pack.release='cb ; dotnet pack --configuration Release'
 
