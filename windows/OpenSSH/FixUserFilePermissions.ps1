cb72d96e01ea3a0afca3645644d95c4756b3ee2e
YJ Park <yjpark@gmail.com>
2020-08-20 13:17:02 +0800

    openssh permission fix on windows

---
+++ windows/OpenSSH/FixUserFilePermissions.ps1
@@ -0,0 +1,18 @@
+﻿[CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact="High")]
+param ()
+Set-StrictMode -Version 2.0
+If (!(Test-Path variable:PSScriptRoot)) {$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition}
+
+Import-Module $PSScriptRoot\OpenSSHUtils -Force
+
+if(Test-Path ~\.ssh\config -PathType Leaf)
+{
+    Repair-UserSshConfigPermission -FilePath ~\.ssh\config @psBoundParameters
+}
+
+Get-ChildItem ~\.ssh\* -Include "id_rsa","id_dsa" -ErrorAction SilentlyContinue | % {
+    Repair-UserKeyPermission -FilePath $_.FullName @psBoundParameters
+}
+
+Write-Host "   Done."
+Write-Host " "
---
+++ windows/OpenSSH/README.md
@@ -0,0 +1,21 @@
+Get the files from
+- https://github.com/PowerShell/Win32-OpenSSH/releases
+
+To run this, need to change policy before
+
+```
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+```
+
+- https://my.oschina.net/u/4319036/blog/3248732
+
+```
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+
+import-Module .\\OpenSSHUtils.psm1
+
+Repair-UserSshConfigPermission ~/.ssh/config
+Get-ChildItem ~\.ssh\* -Include "id_rsa","id_dsa" -ErrorAction SilentlyContinue | % {
+    Repair-UserKeyPermission -FilePath $_.FullName @psBoundParameters
+}
+```
