e834189bcf72b45f8972d1d317be413b97cdc3aa
YJ Park <yjpark@gmail.com>
2020-08-22 22:31:09 +0800

    add a few powershell modules

--- windows/PowerShell/bootstrap/install-powershell-modules.ps1
+++ windows/PowerShell/bootstrap/install-powershell-modules.ps1
@@ -2,4 +2,8 @@ Install-Module oh-my-posh -Scope CurrentUser
 Install-Module posh-git -Scope CurrentUser
 
 Install-Module PSReadLine -Scope CurrentUser
+
 Install-Module PSBookmark -Scope CurrentUser
+Install-Module ZLocation -Scope CurrentUser
+Install-Module PSFzf -Scope CurrentUser
+
