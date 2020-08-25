c3e5e57b03e8e2309593f02fa4de3b3101f78a75
YJ Park <yjpark@gmail.com>
2020-08-25 16:39:01 +0800

    update windows settings

--- fish/config.linux.ubuntu/windows-wsl.fish
+++ fish/config.linux.ubuntu/windows-wsl.fish
@@ -5,17 +5,7 @@ if grep -q microsoft /proc/version
     set -x LS_COLORS ""
     set -x DISPLAY "127.0.0.1:0.0"
 
-    set -x DOCKER_HOST tcp://127.0.0.1:2375
-
-    #alias yarn 'yarn --cache-folder ~/.cache/yarn/v1'
-    #alias dotnet '/mnt/c/Program\ Files/dotnet/dotnet.exe'
-    #alias paket='.paket/paket.exe'
-    #alias fake '~/win/.dotnet/tools/fake.exe'
-
-    alias term 'cd ~/temp ; nohup terminator &'
-
-    # set -x SPACEFISH_CHAR_SYMBOL ">"
-
+    #set -x DOCKER_HOST tcp://127.0.0.1:2375
     # https://www.turek.dev/post/fix-wsl-file-permissions/
     umask 0002
 end
