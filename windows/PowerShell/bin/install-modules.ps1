1dede1286ef84e9bdf7d7ddbaf50d47d596128e7
YJ Park <yjpark@gmail.com>
2020-09-04 16:33:34 +0800

    fix

--- windows/PowerShell/bin/install-modules.ps1
+++
@@ -1,6 +0,0 @@
-dotnet tool install --global paket
-dotnet tool install --global fake-cli
-dotnet tool install --global dotnet-search
-dotnet tool install --global fabulous-cli
-dotnet tool install --global dotMorten.OmdGenerator
-
