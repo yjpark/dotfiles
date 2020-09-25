91522feee6b57e5d6c4b4544c5403fb0e5fc5b14
YJ Park <yjpark@gmail.com>
2020-09-25 09:51:44 +0800

    add tye to dotnet tools

--- windows/PowerShell/bin/install-dotnet-tools.ps1
+++ windows/PowerShell/bin/install-dotnet-tools.ps1
@@ -3,4 +3,4 @@ dotnet tool install --global fake-cli
 dotnet tool install --global dotnet-search
 dotnet tool install --global fabulous-cli
 dotnet tool install --global dotMorten.OmdGenerator
-
+dotnet tool install --global Microsoft.Tye --version "0.4.0-alpha.20371.1"
--- windows/PowerShell/bin/update-dotnet-tools.ps1
+++ windows/PowerShell/bin/update-dotnet-tools.ps1
@@ -2,4 +2,5 @@ dotnet tool update --global fake-cli
 dotnet tool update --global dotnet-search
 dotnet tool update --global fabulous-cli
 dotnet tool update --global dotMorten.OmdGenerator
+dotnet tool update --global Microsoft.Tye --version "0.4.0-alpha.20371.1"
 
