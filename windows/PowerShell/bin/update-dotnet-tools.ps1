646b6c5229d1ac58408a6dd84fa0bd6a676072a9
YJ Park <yjpark@gmail.com>
2020-10-28 20:37:00 +0800

    install fable3 beta tools

--- windows/PowerShell/bin/update-dotnet-tools.ps1
+++ windows/PowerShell/bin/update-dotnet-tools.ps1
@@ -5,4 +5,5 @@ dotnet tool update --global dotnet-search
 dotnet tool update --global fabulous-cli
 dotnet tool update --global dotMorten.OmdGenerator
 dotnet tool update --global Microsoft.Tye --version "0.4.0-alpha.20371.1"
+dotnet tool update --global fable --version 3.0.0-nagareyama-beta-004
 
