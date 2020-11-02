c97192ab96dcfd838185c2c5e6fff7c062dd276b
YJ Park <yjpark@gmail.com>
2020-11-02 15:22:57 +0800

    tweak fish config

---
+++ fish/config.osx/miniconda.fish
@@ -0,0 +1,8 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -d /usr/local/Caskroom/miniconda/base/bin
+    eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
+
