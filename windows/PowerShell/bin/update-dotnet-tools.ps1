f817b1fe40d2aea0456426773142f8be654261ff
YJ Park <yjpark@gmail.com>
2020-11-20 15:35:36 +0800

    update dotnet tools

--- windows/PowerShell/bin/update-dotnet-tools.ps1
+++ windows/PowerShell/bin/update-dotnet-tools.ps1
@@ -1,9 +1,19 @@
-dotnet tool update --global paket --version 5.245.1
-dotnet tool update --global fake-cli --version 5.20.0
+dotnet tool update --global paket --version 5.257.0
+dotnet tool update --global fake-cli --version 5.20.3
+dotnet tool update --global dotnet-interactive
+dotnet tool update --global capnpc-csharp
 dotnet tool update --global dotnet-try
+dotnet tool update --global dotnet-grpc
 dotnet tool update --global dotnet-search
+dotnet tool update --global dotnet-proj
 dotnet tool update --global fabulous-cli
+dotnet tool update --global Falanx.Tool
 dotnet tool update --global dotMorten.OmdGenerator
-dotnet tool update --global Microsoft.Tye --version "0.4.0-alpha.20371.1"
-dotnet tool update --global fable --version 3.0.0-nagareyama-beta-004
+dotnet tool update --global Amazon.Lambda.Tools
+dotnet tool update --global femto
+dotnet tool update --global ilspycmd
+dotnet tool update --global fantomas-tool
+dotnet tool update --global dotnet-certes
+dotnet tool update --global Microsoft.Tye --version "0.5.0-alpha.20555.1"
+dotnet tool update --global fable --version "3.0.0-nagareyama-rc-008"
 
