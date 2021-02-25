3312a7e5efa9cf8d6768fd01ac9786ce2ddf09bc
YJ Park <yjpark@gmail.com>
2021-02-25 16:12:44 +0800

    use installed dotnet on osx if exist

--- fish/config.osx/dotnet.fish
+++ fish/config.osx/dotnet.fish
@@ -1,4 +1,7 @@
-# alias dotnet /usr/local/share/dotnet/dotnet
+if test -d /usr/local/share/dotnet
+    alias dotnet /usr/local/share/dotnet/dotnet
+end
+
 if test -d ~/.dotnet/tools
     set -x PATH $PATH ~/.dotnet/tools
 end
