a76d1bd07317dcaa9560373192fc2914b1b35774
YJ Park <yjpark@gmail.com>
2021-05-18 13:36:14 +0800

    add alias for cargo nightly

--- fish/aliases/cargo.fish
+++ fish/aliases/cargo.fish
@@ -1,4 +1,5 @@
 alias c "cargo"
+alias cn "cargo +nightly"
 alias cw "cargo watch"
 alias cw.build "cargo watch -s 'clear-buffer ; cargo build'"
 alias cw.run "cargo watch -s 'clear-buffer ; cargo run'"
