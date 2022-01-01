15f6f27ffdc95a68430b5100b9b5cb38193cf6a7
YJ Park <yjpark@gmail.com>
2022-01-02 04:51:03 +0800

    cleanup windows setup

--- windows/Applications/bin/install-powershell-modules.ps1
+++ windows/Applications/bin/install-powershell-modules.ps1
@@ -1,2 +1,8 @@
-# https://github.com/dahlbyk/posh-git
-PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+Install-Module PSReadLine -Scope CurrentUser
+
+Install-Module PSBookmark -Scope CurrentUser
+Install-Module ZLocation -Scope CurrentUser
+Install-Module PSFzf -Scope CurrentUser
+
+Install-Module DirColors
+Install-Module posh-git -Scope CurrentUser
